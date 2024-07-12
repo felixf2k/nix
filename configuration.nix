aeb5169db44cf57cd7450bc2bff937eed2b6aee5
Felix Fischerkeller <felixfk2000@gmail.com>
2024-07-12 09:38:29 +0200

    13 current  2024-07-12 09:38:28  24.05.2662.249fbde2a178  6.6.37                          *

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.      
     git
+    gnome3.gnome-tweaks
   #  wget
   ];
 
