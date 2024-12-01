c881d20e24f31449cba52e69b6c6c03d936ffd66
Felix Fischerkeller <felixfk2000@gmail.com>
2024-12-01 21:06:12 +0100

    27 current  2024-12-01 20:49:52  24.05.6850.7e1ca67996af      6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -133,11 +133,11 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.      
     git
     gnome3.gnome-tweaks
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        rust-lang.rust-analyzer
-      ]
-    })
+    # (vscode-with-extensions.override {
+    #  vscodeExtensions = with vscode-extensions; [
+    #    rust-lang.rust-analyzer
+    # ]
+    #})
   #  wget
   ];
 
