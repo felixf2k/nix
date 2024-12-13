26a4648b4f81d750bb2ee092421caa9d50845c4b
Felix Fischerkeller <f.fischerkeller@ims360.de>
2024-12-13 13:21:07 +0100

    38 current  2024-12-13 13:21:04  24.05.6850.7e1ca67996af      6.6.63                          *

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.      
     git
+    rustc
     gnome3.gnome-tweaks
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
