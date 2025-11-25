e2d58b6252515a7376e254f0bddfa8aa874574b1
Felix Fischerkeller <f.fischerkeller@ims360.de>
2025-11-25 20:31:44 +0100

    88 current  2025-11-25 20:31:39  25.05.809873.9a094440e02a    6.12.47                          *

--- configuration.nix
+++ configuration.nix
@@ -196,15 +196,9 @@ in
       pnpm
       nodejs_24
       jetbrains.webstorm
+      steam
     ];
   };
-
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  # };
 
   # enable docker
   virtualisation.docker.enable = true;
